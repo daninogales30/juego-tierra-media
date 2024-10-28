c80e1cd4a682f3b9bf9c94ba1426f78970faca0e
daninogales30 <dnogmat3011@fp.iesromerovargas.com>
2024-10-28 09:26:24 +0100

    Casi terminado la parte de dario y dani

--- parte_final/ejercicio_completo.py
+++ parte_final/ejercicio_completo.py
@@ -1,4 +1,5 @@
-from parte_dani import *
-from parte_dario import *
-from parte_jesus import *
+from parte_dani.parte_dani import *
+from parte_dario.parte3_dario import *
+from parte_jesus.parte_jesus import *
 
+menu()
