5aeea17f31691e17363b73522844f88610a08e40
daninogales30 <dnogmat3011@fp.iesromerovargas.com>
2024-10-28 14:22:54 +0100

    Casi terminado la parte de dario y dani

--- parte_final/ejercicio_completo.py
+++ parte_final/ejercicio_completo.py
@@ -4,36 +4,43 @@ personajes = {}
 ubicaciones = ['rivendel', 'hobbiton', 'minastirith', 'mordor', 'isengard', 'bosquenegro', 'lothlórien']
 armas={
     "anduril":{
+        "nombre": "Anduril",
         "tipo": "Espada",
         "potencia": 80,
         "descripcion": "La espada legendaria de Aragorn, forjada de los fragmentos de Narsil."
     },
     "arcodegaladriel": {
+        "nombre": "Arco de Galadriel",
         "tipo": "Arco",
         "potencia": 70,
         "descripcion":"El arco de Legolas, dado como un regalo por la dama Galadriel."
     },
     "hachadegimli": {
+        "nombre": "Hacha de Gimli",
         "tipo": "Hacha",
         "potencia": 75,
         "descripcion": "El arma favorita del enano Gimli, eficaz en combate cuerpo a cuerpo."
     },
     "dagadefrodo": {
+        "nombre": "Daga de frodo",
         "tipo": "Daga",
         "potencia": 40,
         "descripcion": "Daga élfica que Frodo lleva durante su viaje."
     },
     "baculodesaruman": {
+        "nombre": "Baculo de Saruman",
         "tipo": "Bastón",
         "potencia": 90,
         "descripcion": "Un bastón de poder usado por el mago Saruman."
     },
     "anillounico": {
+        "nombre": "Anillo Unico",
         "tipo": "Objeto especial",
         "potencia": 100,
         "descripcion": "El Anillo creado por Sauron para gobernar todos los demás."
     },
     "espadadeboromir": {
+        "nombre": "Espada de Borimir",
         "tipo": "Espada",
         "potencia": 70,
         "descripcion": "El arma usada por Boromir, capitán de Gondor."
@@ -42,8 +49,8 @@ armas={
 
 
 def is_nombre(nombre):
-    while nombre.lower().strip() not in personajes.keys():
-        print("El nombre no existe")
+    while nombre.lower() not in personajes:
+        print("El nombre no existe o está vacío. Intente nuevamente.")
         nombre = input("Ingrese el nombre: ")
 
     return nombre
@@ -56,13 +63,13 @@ def is_ubicacion(ubicacion):
     return ubicacion
 
 def is_arma(arma):
-    arma_sin_espacios = arma.replace(" ","")
-    while arma_sin_espacios not in armas:
-        print("El arma no existe")
+    arma_sinespacios = arma.replace(" ", "").lower()
+    while arma_sinespacios not in armas:
+        print("El arma no existe.")
         arma = input("Ingrese nuevamente el arma: ")
-        arma_sin_espacios = arma.replace(" ","")
+        arma_sinespacios = arma.replace(" ", "").lower()
 
-    return arma_sin_espacios
+    return arma_sinespacios
 
 def is_faccion(faccion):
     while faccion.strip().lower() not in facciones:
@@ -81,6 +88,9 @@ def is_raza(raza):
 
 def agregar_personajes():
     nombre = input("Ingrese el nombre del personaje: ").strip().lower()
+    while nombre=="":
+        nombre = input("Ingrese el nombre del personaje correctamente: ").strip().lower()
+
     raza = is_raza(input("Ingrese la raza del personaje: "))
     faccion = is_faccion(input("Ingrese la faccion del personaje: "))
     ubicacion = is_ubicacion(input("Ingrese la ubicacion del personaje: "))
@@ -91,7 +101,8 @@ def agregar_personajes():
         "faccion": faccion.lower(),
         "ubicacion": ubicacion.lower(),
         "equipamiento": [],
-        "relaciones": []
+        "relaciones": [],
+        "arma_equipada": None
     }
     print("Personaje añadido\n")
 
@@ -106,20 +117,29 @@ def mostrar_personajes(personajes_diccionario):
         print("Ubicacion:", datos["ubicacion"].capitalize())
 
         if datos["equipamiento"]:
-            equipamiento_descripciones = [f"{arma['tipo']} - {arma['descripcion']}" for arma in datos["equipamiento"]]
-            print("Equipamiento:", ", ".join(equipamiento_descripciones))
+            print("Equipamiento:")
+            for equipamiento in datos["equipamiento"]:
+                print(f"    -Nombre: {equipamiento['nombre']}")
+                print(f"    -Tipo: {equipamiento['tipo']}")
+                print(f"    -Potencia: {equipamiento['potencia']}")
+                print(f"    -Descripcion: {equipamiento['descripcion']}")
+
         else:
             print("Equipamiento: Ninguno")
 
         if datos["relaciones"]:
             print("Relaciones:")
             for relacion in datos["relaciones"]:
-                print(f"- Nombre: {relacion['nombre_relacionado']}")
-                print(f"- Raza: {relacion['tipo']}")
-                print(f"- Faccion: {relacion['nivel_confianza']}\n")
+                print(f"    -Nombre: {relacion['nombre_relacionado']}")
+                print(f"    -Raza: {relacion['tipo']}")
+                print(f"    -Faccion: {relacion['nivel_confianza']}\n")
         else:
-            print("Sin relaciones")
+            print("Relaciones: Ninguno")
         print()
+
+
+
+
 
 def cambiar_localizacion():
     nombre_personaje = is_nombre(input("Ingrese el nombre del personaje: "))
@@ -169,21 +189,24 @@ def anadir_equipamiento():
 
     nombre_arma = is_arma((input("Indique el nombre del arma: ")))
 
-    personajes[nombre_personaje]["equipamiento"].append(armas[nombre_arma])
-    print(f"El arma ha sido añadido al personaje: {nombre_personaje}")
+    personajes[nombre_personaje]["equipamiento"].append({
+        'nombre': armas[nombre_arma]['nombre'],
+        'tipo': armas[nombre_arma]['tipo'],
+        'potencia': armas[nombre_arma]['potencia'],
+        'descripcion': armas[nombre_arma]['descripcion'],
+    })
+    print(f"El arma: {armas[nombre_arma]['nombre']}  sido añadido al personaje: {nombre_personaje}")
 
 def equipar_arma():
-    nombre_personaje = ""
-    is_nombre(nombre_personaje)
+    nombre_personaje = is_nombre(input("Indique el nombre del personaje: "))
 
-    nombre_arma = ""
-    is_arma(nombre_arma)
+    nombre_arma = is_arma(input("Indique el nombre del arma: "))
 
-    if armas[nombre_arma] not in personajes[nombre_personaje]["equipamiento"]:
-        raise ValueError(f"Este arma no esta en el equipamiento del personaje {nombre_personaje}")
+    while nombre_arma not in personajes[nombre_personaje]["equipamiento"]:
+        nombre_arma = input("Indique el nombre del arma: ")
 
-    personajes[nombre_personaje]["equipamiento"]=armas[nombre_arma]
-    print(f"El personaje {nombre_personaje} ha cambiado de arma {armas[nombre_arma]}")
+    personajes[nombre_personaje]["arma_equipada"] = armas[nombre_arma]
+    print(f"El personaje {nombre_personaje} ha equipado el arma: {nombre_arma}")
 
 
 def simular_batalla():
@@ -204,6 +227,7 @@ def simular_batalla():
     print(f"La batalla acaba de empezar entre {personaje1} y {personaje2}")
     print(f"{personaje1}con {arma1['tipo']}vs {personaje2}con {arma2['tipo']}")
     print(f"Y el ganador es:")
+
 
 def menu():
     es_valido = True
@@ -246,11 +270,12 @@ def menu():
                 case 9:
                     mostrar_personajes(personajes)
                 case 10:
-                    print("Saliendo del juego")
-                    es_valido = False
+                    confirmacion = input("¿Está seguro que desea salir? (s/n): ").strip().lower()
+                    if confirmacion == 's':
+                        print("Saliendo del juego...")
+                        es_valido = False
         except ValueError as e:
             print(f"Error: {e}")
-
 
 def listar_personajes_faccion(personajes):
     faccion = is_faccion(input("Ingrese la facción que desea listar: "))
